62cc36267a2fbc7ee03e4639b68f3499adc5a595
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-04 03:33:06 +0300

    Init NextApp, баз. настр.

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,36 @@ export default function Home() {
           />
         </div>
 
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
         <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
