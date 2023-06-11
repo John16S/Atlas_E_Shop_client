6cd9a9c92007d8b0f216fd4cc41071a28bc53045
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-12 02:22:42 +0300

    Сортировка(first: cheap/expensice/popular)

--- components/modules/Catalog/FilterSelect.tsx
+++ components/modules/Catalog/FilterSelect.tsx
@@ -1,22 +1,96 @@
-import Select from "react-select"
-import { $mode } from "@/context/mode"
-import { useStore } from "effector-react"
-import { useState } from "react"
-import { SelectOptionType } from "@/types/common"
-import { createSelectOption } from "@/utils/common"
-import { controlStyles, menuStyles, selectStyles } from "@/styles/catalog/select"
-import { optionStyles } from "@/styles/searchInput"
-import { categoriesOptions } from "@/utils/selectContents"
+import Select from 'react-select'
+import { $mode } from '@/context/mode'
+import { useStore } from 'effector-react'
+import { useEffect, useState } from 'react'
+import { IOption, SelectOptionType } from '@/types/common'
+import { createSelectOption } from '@/utils/common'
+import {
+    controlStyles,
+    menuStyles,
+    selectStyles,
+} from '@/styles/catalog/select'
+import { optionStyles } from '@/styles/searchInput'
+import { categoriesOptions } from '@/utils/selectContents'
+import {
+    $goods,
+    setGoodsByPopularity,
+    setGoodsCheapFirst,
+    setGoodsExpensiveFirst,
+} from '@/context/goods'
+import { useRouter } from 'next/router'
 
 const FilterSelect = () => {
+    const router = useRouter()
     //стили для тёмный темы
     const mode = useStore($mode) //получаем доступ к состоянию mode
+
+    const goods = useStore($goods) //получаем доступ к состоянию mode
     
     const [categoryOption, setCategoryOption] = useState<SelectOptionType>(null)
 
-    //*Функция для установки выбранного значения из выпадающего списка
-    const handleCategoryOptionChange = (selectedOption: SelectOptionType) => {
+    useEffect(() => {
+        if(goods.rows){ //Если товары уже загрузились из сервера...
+            //только тогда делать сортировку
+            switch (router.query.first) {
+                case 'cheap':
+                    updateCategoryOption('Сначала дешевые')
+                    setGoodsCheapFirst()
+                    break
+                case 'expensive':
+                    updateCategoryOption('Сначала дорогие')
+                    setGoodsExpensiveFirst()
+                    break
+                case 'popular':
+                    updateCategoryOption('По популярности')
+                    setGoodsByPopularity()
+                    break
+    
+                default:
+                    updateCategoryOption('Сначала дешевые')
+                    setGoodsCheapFirst()
+                    break
+            }
+        }
+    }, [goods.rows, router.query.first])
+
+    //*Фнкция чтобы поле сортировки сохранял свое значение при перезпгрузки
+    const updateCategoryOption = (value: string) =>
+        setCategoryOption({ value, label: value })
+
+    //*Функция чтобы при перезагрузки стр. сохраился наша сортировка
+    const updateRouteParam = (first: string) =>
+        router.push(
+            {
+                query: {
+                    ...router.query,
+                    first,
+                },
+            },
+            undefined,
+            { shallow: true }
+        )
+
+    //*Функция для установки выбранного значения из выпадающего списка сортировки
+    const handleSortOptionChange = (selectedOption: SelectOptionType) => {
         setCategoryOption(selectedOption)
+
+        switch ((selectedOption as IOption).value) {
+            case 'Сначала дешевые':
+                setGoodsCheapFirst()
+                updateRouteParam('cheap')
+                break
+            case 'Сначала дорогие':
+                setGoodsExpensiveFirst()
+                updateRouteParam('expensive')
+                break
+            case 'По популярности':
+                setGoodsByPopularity()
+                updateRouteParam('popular')
+                break
+
+            default:
+                break
+        }
     }
 
     // импортируем Select из библиотеки react-select
@@ -24,28 +98,27 @@ const FilterSelect = () => {
         <Select
             placeholder="Я ищу..."
             value={categoryOption || createSelectOption('Сначала дешевые')}
-            onChange={handleCategoryOptionChange}
+            onChange={handleSortOptionChange}
             styles={{
                 ...selectStyles,
                 control: (defaultStyles) => ({
-                    ...controlStyles(defaultStyles, mode)
+                    ...controlStyles(defaultStyles, mode),
                 }),
                 input: (defaultStyles) => ({
                     ...defaultStyles,
-                    color: mode === 'dark' ? '#f2f2f2' : '#222222'
+                    color: mode === 'dark' ? '#f2f2f2' : '#222222',
                 }),
                 menu: (defaultStyles) => ({
-                    ...menuStyles(defaultStyles, mode)
+                    ...menuStyles(defaultStyles, mode),
                 }),
                 option: (defaultStyles, state) => ({
-                    ...optionStyles(defaultStyles, state, mode)
+                    ...optionStyles(defaultStyles, state, mode),
                 }),
             }}
             isSearchable={false}
-            options = {categoriesOptions}
+            options={categoriesOptions}
         />
     )
-
 }
 
 export default FilterSelect
--- context/goods.ts
+++ context/goods.ts
@@ -5,6 +5,31 @@ const goods = createDomain()
 
 export const setGoods = goods.createEvent<IGoods>()
 
+//*Создаём 3 event для каждого состояния сортироваки
+export const setGoodsCheapFirst = goods.createEvent()
+export const setGoodsExpensiveFirst = goods.createEvent()
+export const setGoodsByPopularity = goods.createEvent()
+
 export const $goods = goods
     .createStore<IGoods>({} as IGoods)
     .on(setGoods, (_, goods) => goods)
+    .on(setGoodsCheapFirst, (state) => ({
+        ...state,
+        rows: state.rows.sort((a, b) => a.price - b.price),
+    }))
+    .on(setGoodsExpensiveFirst, (state) => ({
+        ...state,
+        rows: state.rows.sort((a, b) => b.price - a.price),
+    }))
+    .on(setGoodsByPopularity, (state) => ({
+        ...state,
+        rows: state.rows.sort((a, b) => {
+            if (a.bestseller && !b.bestseller) {
+                return -1; // a - true, b - false, a должен быть первым
+            }
+            if (!a.bestseller && b.bestseller) {
+                return 1; // a - false, b - true, b должен быть первым
+            }
+            return 0; // значения равны, порядок не важен
+        }),
+    }))
