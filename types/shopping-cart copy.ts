53cad4ab5c16aa5a91db2e689231de11cf9879ee
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-13 18:46:24 +0300

    started AddToCart

---
+++ types/shopping-cart copy.ts
@@ -0,0 +1,35 @@
+export interface IShoppingCartItem {
+    id: number
+    userId: number
+    goodId: number
+    name: string
+    price: number
+    size: string
+    image: string
+    category: string
+    subcategory: string
+    count: number
+    totalPrice: number
+}
+
+export interface IAddToCartFx {
+    url: string
+    username: string
+    goodId: number
+}
+
+export interface IUpdateCartItemFx {
+    url: string
+    payload: {
+        totalPrice?: number
+        count?: number
+    }
+}
+
+export interface ICartItemCounterProps {
+    totalCount: number
+    goodId: number
+    initialCount: number
+    increasePrice: VoidFunction
+    decreasePrice: VoidFunction
+}
