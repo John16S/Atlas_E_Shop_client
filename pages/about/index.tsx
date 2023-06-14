074ebe42d29f5832024e1b8ead01f333700576a7
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-06-14 08:23:01 +0300

    AboutPage (TODO: images)

---
+++ pages/about/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import { useCallback } from 'react'
+import Layout from '@/components/layout/Layout'
+import AboutPage from '@/components/templates/AboutPage/AboutPage'
+
+function About() {
+  const getDefaultTextGenerator = useCallback(() => 'О компании', [])
+  const getTextGenerator = useCallback((param: string) => ({}[param]), [])
+
+  return (
+    <>
+      <Head>
+        <title>Атлас | О нас</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.svg" />
+      </Head>
+      <Layout>
+        <main>
+          {/* <Breadcrumbs
+            getDefaultTextGenerator={getDefaultTextGenerator}
+            getTextGenerator={getTextGenerator}
+          /> */}
+          <AboutPage />
+          <div className="overlay" />
+        </main>
+      </Layout>
+    </>
+  )
+}
+
+export default About
